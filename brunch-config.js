3a06400f7923406a9a1e84ff2bc23b14e9f20558
zhirong.li <zhirong.li@alo7.com>
2017-07-20 11:17:09 +0800

    设置config

--- brunch-config.js
+++ brunch-config.js
@@ -6,23 +6,47 @@ exports.files = {
       'app.js': /^app/
     }
   },
-  stylesheets: {joinTo: 'app.css'}
+  stylesheets: { joinTo: 'app.css' }
 };
 
 exports.plugins = {
-  babel: {presets: ['latest']},
+  babel: { presets: ['latest'] },
   handlebars: {
-      overrides: handlebars => {
-        handlebars.JavaScriptCompiler.prototype.nameLookup = (parent, name, type) => {
-          // Your custom nameLookup method.
-        }
-      },
-      include: {
-        runtime: false // include the full compiler javascript
-      },
-      pathReplace: /0^/, // match nothing, use full file path for module name
-      locals: {
-        // static data
+    overrides: handlebars => {
+      handlebars.JavaScriptCompiler.prototype.nameLookup = (parent, name, type) => {
+        // Your custom nameLookup method.
       }
+    },
+    include: {
+      runtime: false // include the full compiler javascript
+    },
+    pathReplace: /0^/, // match nothing, use full file path for module name
+    locals: {
+      // static data
     }
+  }
+  // static: {
+  //   processors: [
+  //     require('html-brunch-static') {
+  //       processors: /\/partials\//,
+  //       partials: /\/partials\//,
+  //       layouts: /\/layouts\//,
+  //       handlebars: {
+  //         enableProcessor:true,
+  //         helpers: { }
+  //       }
+  //     }
+  //   ]
+
+  // }
 };
+exports.hooks={
+  preCompile() {
+      console.log("About to compile...");
+      return Promise.resolve();
+  },
+  onCompile(generatedFiles, changedAssets) {
+      console.log(generatedFiles.map(f => f.path));
+  }
+}
+
